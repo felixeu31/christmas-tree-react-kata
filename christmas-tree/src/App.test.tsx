6d255839933e9136aa05be934e43edc18200332b
Félix Díez Arias <fdiez@tunstall.es>
2023-09-12 09:17:59 +0200

    RED: should render TreeGenerator and pass treeHeight as a prop

--- christmas-tree/src/App.test.tsx
+++ christmas-tree/src/App.test.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import TreeGenerator from './TreeGenerator';
+
+jest.mock('./TreeGenerator', () => {
+  return function MockedTreeGenerator(props: any) {
+    return (
+      <div data-testid="tree-generator">
+        <p data-testid="tree-height">{props.treeHeight}</p>
+      </div>
+    );
+  };
+});
 
 describe('Christmas tree App', () => {
   it('should render app title', () => {
@@ -16,5 +27,22 @@ describe('Christmas tree App', () => {
   it('should render a generate button', () => {
     const { getByRole } = render(<App />);
     expect(getByRole('button')).toBeInTheDocument();    
+  });
+
+  it('should render TreeGenerator and pass treeHeight as a prop', () => {
+    const { getByPlaceholderText, getByText, getByTestId } = render(<App />);
+    const input = getByPlaceholderText('Indicate the desired tree height');
+    const generateButton = getByText('Generate');
+
+    fireEvent.change(input, { target: { value: '10' } });
+    fireEvent.click(generateButton);
+
+    // Check if the TreeGenerator component is rendered
+    const treeGenerator = getByTestId('tree-generator');
+    expect(treeGenerator).toBeInTheDocument();
+
+    // Check if the treeHeight is passed as a prop to TreeGenerator
+    const treeHeightElement = getByTestId('tree-height');
+    expect(treeHeightElement.textContent).toBe('10');
   });
 });
