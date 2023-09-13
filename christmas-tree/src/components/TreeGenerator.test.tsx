2a0f34047ae1a4547fd4c33698eaf9a79bee2eb0
Félix Díez Arias <fdiez@tunstall.es>
2023-09-13 18:22:11 +0200

    RED: should render tree of height 1

--- christmas-tree/src/components/TreeGenerator.test.tsx
+++ christmas-tree/src/components/TreeGenerator.test.tsx
@@ -4,6 +4,17 @@ import TreeGenerator from "./TreeGenerator";
 describe('Tree generator', () => {
     it('should render warning message when height is 0', () => {
       const { getByText } = render(<TreeGenerator treeHeight={0}/>);
+      
       expect(getByText('Please, select height and generate to get your tree')).toBeInTheDocument();
+    });
+    
+    it('should render tree of height 1', () => {
+        const { getByTestId } = render(<TreeGenerator treeHeight={1}/>);
+
+        const renderedTree = getByTestId("tree");
+
+        expect(renderedTree).toBeInTheDocument();
+        expect(renderedTree.innerHTML).toBe('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;X<br>'+
+        '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|')
     });
 });
